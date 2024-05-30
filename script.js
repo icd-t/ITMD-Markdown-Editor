eb4dee2deda3f93f178b6954f1d92d147673f31a
icd-t <139998388+icd-t@users.noreply.github.com>
2024-05-30 00:45:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -63,8 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
             return false;
         }
     };
-
-    // Extend marked tokenizer
     const originalTokenizer = { ...marked.Lexer.prototype.tokenizer };
     marked.use({ tokenizer: { ...originalTokenizer, ...tokenizer } });
 
@@ -72,7 +70,6 @@ document.addEventListener("DOMContentLoaded", function() {
         fences(src) {
             const match1 = src.match(/^\::+([^\$\n]+?)\::+/);
             if (match1) {
-                // Preserve newlines by wrapping code in <pre> without escaping
                 const code = match1[1].trim().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '&#10;');
                 return {
                     type: 'html',
@@ -84,12 +81,13 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
 
+    const originalTokenizer2 = { ...marked.Lexer.prototype.tokenizer };
+    marked.use({ tokenizer: { ...originalTokenizer2, ...tokenizer2 } });
+
     const tokenizer3 = {
         fences(src) {
-            // Regular expression to match text enclosed in double hashtags
-            const match1 = src.match(/^##([^#\n]+)##/);
+            const match1 = src.match(/^##([^#]+)##/);
             if (match1) {
-                // Preserve newlines by wrapping code in <pre> without escaping
                 const code = match1[1].trim().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '&#10;');
                 return {
                     type: 'html',
@@ -162,4 +160,24 @@ function closeHandler(e) {
     window.addEventListener('beforeunload', closeHandler, true);
   } else if (window.attachEvent) {
     window.attachEvent('onbeforeunload', closeHandler);
+}
+
+// Get the button:
+let mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
